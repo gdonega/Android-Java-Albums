3b17f22a574558afa8c81d651484f9c7b25305f8
Gustavo Donega Queiroz <name>
2018-02-13 15:40:57 -0200

    All function ok

--- app/src/main/java/informatica/senai/sp/br/albumsnroll/LoginActivity.java
+++ app/src/main/java/informatica/senai/sp/br/albumsnroll/LoginActivity.java
@@ -5,23 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.facebook.stetho.Stetho;
 
+import informatica.senai.sp.br.albumsnroll.view.activitys.EditActivity;
+import informatica.senai.sp.br.albumsnroll.view.activitys.Main;
 import informatica.senai.sp.br.albumsnroll.view.activitys.MenuActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
-    EditText user;
-    EditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        user = findViewById(R.id.userInput);
-        pass = findViewById(R.id.passInput);
 
         // Create an InitializerBuilder
         Stetho.InitializerBuilder initializerBuilder =
@@ -45,7 +43,8 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    public void createNewUser(View view) {
+    public void newAlbum(View view) {
+        startActivityForResult((new Intent(Main.getContext(), EditActivity.class)), MenuActivity.EDIT);
     }
 
     public void login(View view) {
@@ -55,4 +54,28 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case MenuActivity.EDIT:
+                switch (resultCode) {
+                    case RESULT_OK:
+                        Toast.makeText(this, data.getStringExtra(MenuActivity.EDIT_RESULT), Toast.LENGTH_SHORT).show();
+                        break;
+                    case RESULT_CANCELED:
+
+                        if (data != null) {
+                            Toast.makeText(this, data.getStringExtra(MenuActivity.EDIT_RESULT), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                }
+                break;
+        }
+
+
+    }
 }
--- app/src/main/java/informatica/senai/sp/br/albumsnroll/view/fragments/recyclerView/RVManager.java
+++ app/src/main/java/informatica/senai/sp/br/albumsnroll/view/fragments/recyclerView/RVManager.java
@@ -1,5 +1,6 @@
 package informatica.senai.sp.br.albumsnroll.view.fragments.recyclerView;
 
+import android.content.res.Configuration;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -8,13 +9,16 @@ import java.util.List;
 
 import informatica.senai.sp.br.albumsnroll.logic.dao.AlbumDao;
 import informatica.senai.sp.br.albumsnroll.view.activitys.Main;
+import informatica.senai.sp.br.albumsnroll.view.activitys.MenuActivity;
 
 /**
  * Created by gustavo on 12/02/2018.
  */
 
 public class RVManager {
-    /**Variables**/
+    /**
+     * Variables
+     **/
     public static final RVManager INSTANCE = new RVManager();
     private AlbumAdaper albumAdaper = new AlbumAdaper();
     private RecyclerView recyclerView;
@@ -22,6 +26,7 @@ public class RVManager {
 
     /**
      * RETURN THE INSTANCE OF THIS CLASS
+     *
      * @return RVManager
      */
     public RVManager getInstance() {
@@ -31,20 +36,31 @@ public class RVManager {
 
     /**
      * SET A RecyclerView TO MANAGER
+     *
      * @param rv = RecyclerView instance
      */
     public void setRecyclerView(RecyclerView rv) {
         this.recyclerView = rv;
         this.recyclerView.setAdapter(this.albumAdaper);
-        this.listForm();
+        this.listForm("grid");
     }
 
     /**
      * LayoutManager OF THE LIST
      */
-    public void listForm() {
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(Main.getContext(), 3);
-        recyclerView.setLayoutManager(layoutManager);
+    public void listForm(String listForm) {
+
+        switch (listForm) {
+            case "grid":
+            case "Grid":
+                int orientacao = Main.getContext().getResources().getConfiguration().orientation;
+                if (orientacao == Configuration.ORIENTATION_PORTRAIT) {
+                    recyclerView.setLayoutManager(new GridLayoutManager(Main.getContext(), 3));
+                } else {
+                    recyclerView.setLayoutManager(new GridLayoutManager(Main.getContext(), 5));
+                }
+        }
+
     }
 
 
@@ -57,6 +73,7 @@ public class RVManager {
         this.canCheck = statusOfCheckBox;
         this.notifyToRemoveItems();
     }
+
     public Boolean getStatusOfCheckBox() {
         return canCheck;
     }
@@ -65,30 +82,29 @@ public class RVManager {
     /**
      * USE THIS METHOD WHEN YOU JUST WANT TO UPDATE
      * YOUR LIST VIEW
-     *
+     * <p>
      * BE CAREFUL: YOU DON'T NEED TO USE THIS METHOD WHEN
      * YOU USE ANOTHER METHOD TO DELETE FROM THIS CLASS
      */
-    public void notifySomeChangeInTheList(){
+    public void notifySomeChangeInTheList() {
         albumAdaper.notifyDataSetChanged();
     }
 
 
     /**
      * DELETE ITEMS AND UPDATE THE LIST
-     *
+     * <p>
      * USER WHEN: YOU DON'T NEED TO UPDATE ALL THE LIST
-     *
+     * <p>
      * BE CAREFUL: IT DOESN'T HELP WHEN YOU NEED TO UPDATE THE
      * CHECKBOX
-     *
-     *
+     * <p>
+     * <p>
      * WHAT YOU NEED TO CONFIGURE:
      * 1) DO A LOGIC TO ADD AND REMOVE THEITEM POSITION IN
      * THE LIST WHEN USER CLICK IN CHECKBOX
      * 2) USE THE "notifySomeItemsWereRemoved()" WHEN
      * USER CLICK IN DELETE
-     *
      */
     public void notifySomeItemsWereRemoved() {
         dao.deleteAll();
@@ -98,26 +114,29 @@ public class RVManager {
         }
 
     }
+
     private List<Integer> listOfPositionsToDel = new ArrayList<>();
-    public List<Integer> getlistOfPositionsToDel(){
+
+    public List<Integer> getlistOfPositionsToDel() {
         return listOfPositionsToDel;
     }
-    public void addPositionToDel(int positionToDel){
+
+    public void addPositionToDel(int positionToDel) {
         this.listOfPositionsToDel.add(positionToDel);
     }
-    public void removePositionToDel(int positionToDel){
+
+    public void removePositionToDel(int positionToDel) {
         this.listOfPositionsToDel.remove(positionToDel);
     }
 
 
     /**
      * UPDATE THE ENTIRE LIST
-     *
+     * <p>
      * USER WHEN: YOU NEED TO UPDATE ALL THE ITEMS OF THE
      * LIST
-     *
      */
-    public void notifyToRemoveItems(){
+    public void notifyToRemoveItems() {
         dao.deleteAll();
         notifySomeChangeInTheList();
     }
@@ -125,10 +144,11 @@ public class RVManager {
 
     /**
      * DELETE A ITEM AND UPDATE THE LIST
+     *
      * @param positionOfTheItemOnList = Position of the Item in the list
-     * @param idOfTheItemOnList = Id of Item
+     * @param idOfTheItemOnList       = Id of Item
      */
-    public void notifyOneItemRemove(Integer positionOfTheItemOnList, Long idOfTheItemOnList){
+    public void notifyOneItemRemove(Integer positionOfTheItemOnList, Long idOfTheItemOnList) {
         dao.delete(idOfTheItemOnList);
         albumAdaper.notifyItemRemoved(positionOfTheItemOnList);
     }
